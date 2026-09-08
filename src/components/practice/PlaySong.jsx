38410cbe5f0a8b2e4a03c66a100f1ac68a470efa
vinodbangalore87220 <vinodbangalore87220@gmail.com>
2026-09-08 23:25:39 +0530

    add play and pause audio with same image click

--- src/components/practice/PlaySong.jsx
+++ src/components/practice/PlaySong.jsx
@@ -1,25 +1,35 @@
-import React, { useRef } from 'react'
-import Song1 from "../../../public/parvati.mp4"
+import React, { useRef, useState } from "react";
+import parvati from "../../../public/parvati.mp4";
+import mahadev from "../../../public/mahadev.mp4";
 
 const PlaySong = () => {
+  let audioRef = useRef(null);
 
-    let audioRef = useRef(null);
 
-    let playAudio = ()=>{
-        audioRef.current.play();
+  let handleAudio = () => {
+   
+    if(audioRef.current.paused){
+      audioRef.current.play()
     }
-
-    let pauseAudio = ()=>{
-        audioRef.current.pause();
+    else{
+      audioRef.current.pause()
     }
+   
+  } 
 
   return (
     <div>
-      <audio ref={audioRef} src={Song1}></audio>
-      <button onClick={playAudio}>Play</button>
-      <button onClick={pauseAudio}>Pause</button>
-    </div>
-  )
-}
+      <audio ref={audioRef} src={parvati}></audio>
+      <img
+        width="100"
+        height="100"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM5iUPdvba1884QhkKjpk53gk-I3iPof6xDBvQsc0kFyfgAi4SNJgE4mH7&s=10"
+        alt=""
+        onClick={handleAudio}
+      />
 
-export default PlaySong
+    </div>
+  );
+};
+
+export default PlaySong;
